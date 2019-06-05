0a9d68ac616883f4141c4101440b1ccacafa447a
Artem <medvynskyi.artem@gmail.com>
2019-06-05 15:26:49 +0300

    HM #5 switch

---
+++ src/TestMain.java
@@ -0,0 +1,24 @@
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        String str1 = args[0];
+        int marks = Integer.parseInt(str1);
+
+        switch (marks){
+            case 90:
+                System.out.println("mark : "+marks+" is A");
+                break;
+
+            case 80:
+                System.out.println("mark : "+marks+" is B");
+                break;
+
+            case 70:
+                System.out.println("mark : "+marks+" is C");
+                break;
+             default:
+                 System.out.println("Mark : "+marks+" is FAIL");
+        }
+    }
+}
